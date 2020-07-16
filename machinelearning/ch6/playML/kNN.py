46a418e7d2da635208d43897123202a4beafb69f
teng <wwwsousoucom@126.com>
2020-07-16 16:48:05 +0800

    随机

--- machinelearning/ch6/playML/kNN.py
+++ machinelearning/ch6/playML/kNN.py
@@ -1,4 +1,4 @@
-from numpy import argsort, array, sum
+import numpy as np
 from math import sqrt
 from collections import Counter
 from .metrics import accuracy_score
@@ -26,16 +26,16 @@ class KNNClassifier:
         assert self._X_train is not None and self._y_train is not None, "must fit before predict!"
         assert X_predict.shape[1] == self._X_train.shape[1], "the feature number of X_predict must be equal to X_train"
         y_predict = [self._predict(x) for x in X_predict]
-        return array(y_predict)
+        return np.array(y_predict)
 
     def _predict(self, x):
         """给定单个待预测数据x，返回x的预测结果值"""
         assert x.shape[0] == self._X_train.shape[1], \
             "the feature number of x must be equal to X_train"
 
-        distances = [sqrt(sum((x_train - x) ** 2))
+        distances = [sqrt(np.sum((x_train - x) ** 2))
                      for x_train in self._X_train]
-        nearest = argsort(distances)
+        nearest = np.argsort(distances)
 
         topK_y = [self._y_train[i] for i in nearest[:self.k]]
         votes = Counter(topK_y)
--- machinelearning/ch6/playML/metrics.py
+++ machinelearning/ch6/playML/metrics.py
@@ -3,9 +3,11 @@ from math import sqrt
 
 
 def accuracy_score(y_true, y_predict):
-    '''计算y_true和y_predict之间的准确率'''
-    assert y_true.shape[0] == y_predict.shape[0], "the size of y_true must be equal to the size of y_predict"
-    return sum(y_true == y_predict)/len(y_true)
+    """计算y_true和y_predict之间的准确率"""
+    assert len(y_true) == len(y_predict), \
+        "the size of y_true must be equal to the size of y_predict"
+    return np.sum(y_true == y_predict) / len(y_true)
+
 
 def mean_squared_error(y_true, y_predict):
     """计算y_true和y_predict之间的MSE"""
@@ -23,6 +25,8 @@ def root_mean_squared_error(y_true, y_predict):
 
 def mean_absolute_error(y_true, y_predict):
     """计算y_true和y_predict之间的MAE"""
+    assert len(y_true) == len(y_predict), \
+        "the size of y_true must be equal to the size of y_predict"
 
     return np.sum(np.absolute(y_true - y_predict)) / len(y_true)
 
