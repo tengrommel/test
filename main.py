7eacab582df6ab4a490b076e61a63e7077e9df8b
teng <wwwsousoucom@126.com>
2020-08-09 22:43:43 +0800

    GPU的确快

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import torch
 import tensorflow as tf
 
-print("torch GPU:", torch.cuda.is_available())
+if __name__ == '__main__':
+    print("torch GPU:", torch.cuda.is_available())
+    print("tensorflow gpu:", tf.config.list_physical_devices('GPU'))
