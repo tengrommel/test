e3105a247d566f3f8282d00d8b2274e6c0d55c6c
teng <wwwsousoucom@126.com>
2020-08-13 09:29:40 +0800

    search

--- basic/ch1/search.py
+++ basic/ch1/search.py
@@ -6,5 +6,25 @@ def linear_search(list_ex, val):
         return None
 
 
+def binary_search(li, val):
+    left = 0
+    right = len(li) - 1
+    while left <= right:
+        # 候选区有值
+        mid = (left + right) // 2
+        if li[mid] == val:
+            return mid
+        elif li[mid] > val:
+            # 待查找的值在mid左侧
+            right = mid - 1
+        else:
+            # li[mid] < val 待查找的值在mid右侧
+            left = mid + 1
+    else:
+        return None
+
+
 if __name__ == '__main__':
-    print(linear_search([1,2,3], 1))
+    # print(linear_search([1,2,3], 1))
+    li = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(binary_search(li, 3))
