f721af2463db87b657e116baead45dc1f0feb8ce
teng <wwwsousoucom@126.com>
2020-08-13 21:08:55 +0800

    堆排序 代码

--- basic/ch1/heap_sort.py
+++ basic/ch1/heap_sort.py
@@ -24,8 +24,31 @@ def sift(li, low, high):
             j = 2 * i + 1
         else:
             # tmp更大，把tmp放到i的位置上
-            li[i] = tmp # 把tmp放到某一级位置上
+            li[i] = tmp  # 把tmp放到某一级位置上
             break
     else:
         li[i] = tmp
         # 把tmp放到叶子节点上
+
+
+def heap_sort(li):
+    n = len(li)
+    for i in range((n - 2) // 2, -1, -1):
+        # i 表示建堆的时候调整的部分的根的下标
+        sift(li, i, n - 1)
+    # 建堆完成
+    # print(li)
+    for i in range(n - 1, -1, -1):
+        # i 指向当前堆的最后一个元素
+        li[0], li[i] = li[i], li[0]
+        sift(li, 0, i - 1)
+        # i-1是新的high
+
+
+if __name__ == '__main__':
+    li = [i for i in range(100)]
+    import random
+    random.shuffle(li)
+    print(li)
+    heap_sort(li)
+    print(li)
